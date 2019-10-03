c97152f05e73cd0541b5929a6929f90b310344f8
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-10-02 23:50:27 -0400

    Create install-font-fira-code-retina.sh

---
+++ scripts/install-font-fira-code-retina.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# download the font
+cd ~/Downloads
+mkdir tmp
+cd tmp
+curl -s -L https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Retina.ttf?raw=true > FiraCode-Retina.ttf
+
+# install the font by moving it to the font folder
+# to install fonts for all users, copy to `/usr/share/fonts` instead
+mv "FiraCode-Retina.ttf" -t ~/.local/share/fonts
+
+# reload font cache
+fc-cache
