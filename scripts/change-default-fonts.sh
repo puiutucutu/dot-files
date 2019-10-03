89c0dd0ec611bee869032c6d18aa9c39af1bed2f
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-10-03 00:21:27 -0400

    Update change-default-fonts.sh

--- scripts/change-default-fonts.sh
+++ scripts/change-default-fonts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gsettings set org.gnome.desktop.interface monospace-font-name 'Fira Code 10'
-gsettings set org.mate.interface monospace-font-name 'Fira Code Retina 10'
+gsettings set org.mate.interface monospace-font-name 'Fira Code 10'
