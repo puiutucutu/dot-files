c2400719725b2bdfcb4137612f7ffc577e74eae3
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-10-03 23:54:50 -0400

    add example php script executable & update comment

---
+++ scripts/hello-world.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env php
+
+<?php
+
+echo "hello world";
+echo PHP_EOL;
+echo __DIR__;
--- scripts/install-font-fira-code-retina.sh
+++ scripts/install-font-fira-code-retina.sh
@@ -4,8 +4,7 @@
 cd ~/Downloads
 curl -s -L https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Retina.ttf?raw=true > FiraCode-Retina.ttf
 
-# install the font by moving it to the font folder
-# to install fonts for all users, copy to `/usr/share/fonts` instead
+# install the font by moving it to the font folder (to install for all users, copy instead to `/usr/share/fonts`)
 mv "FiraCode-Retina.ttf" -t ~/.local/share/fonts
 
 # reload font cache
